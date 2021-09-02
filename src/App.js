faa1e16ad1014c33906e6f25e75233ddf6ec79a9
DrPayne25 <alex.payne1125@gmail.com>
2021-09-01 21:27:57 -0700

    created the routes for the app as well as setting up a header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import './App.css';
+import Header from './Header.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-class App extends React.Component{
+class App extends React.Component {
   render() {
-    return <h1>Welcome to the Gaming Odyssey</h1>
+    return (
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h1>Welcome to The Gaming Odyssey</h1>
+          </Route>
+          <Route exact path="/about-us">
+            <h1>About the Gaming Odyssey Team</h1>
+          </Route>
+          <Route exact path="/odyssey">
+            <h1>Take a look at upcoming Gaming Odysseys</h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
 
